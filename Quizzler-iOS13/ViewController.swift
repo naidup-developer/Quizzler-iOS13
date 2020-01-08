e0195ab0d55991a11e910b91d9dafe189e9108aa
Naidu <naidup.developer@gmail.com>
2020-01-09 01:38:56 +0530

    questions added

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 struct Question {
     var text : String?
     var answer : String?
+    
+    init(q : String, a: String) {
+        text = q
+        answer = a
+    }
 }
 
 class ViewController: UIViewController {
@@ -24,9 +29,19 @@ class ViewController: UIViewController {
     
     let quiz = [
         
-        Question.init(text: "Four + Two is equal to Six", answer: "True"),
-        Question.init(text: "Five - Three is greater than One", answer: "True"),
-        Question.init(text: "Three + Eight is less than ten", answer: "False")
+          Question(q: "A slug's blood is green.", a: "True"),
+              Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+              Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+              Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+              Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+              Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+              Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+              Question(q: "Google was originally called 'Backrub'.", a: "True"),
+              Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+              Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+              Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+              Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+
     ]
     
     override func viewDidLoad() {
